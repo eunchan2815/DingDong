ba6642f17a1e89f25f267a742738253a7cb27cc7
김은찬 <164847037+eunchan2815@users.noreply.github.com>
2025-08-10 23:26:55 +0900

    Initial Commit

---
+++ DingDong/DingDongApp.swift
@@ -0,0 +1,17 @@
+//
+//  DingDongApp.swift
+//  DingDong
+//
+//  Created by 김은찬 on 8/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DingDongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
